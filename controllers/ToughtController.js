6827f90a353dae7984ded23c430e56079fa67a36
gabrielleeee <gabi.poletti2019@gmail.com>
2024-02-27 09:54:17 -0300

    feat: funcionalidade de atualização de pensamento

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -85,4 +85,26 @@ module.exports = class ToughtController {
 
     res.render('toughts/edit', { tought })
   }
+
+  static async updateToughtSave (req, res) {
+
+    const id = req.body.id
+
+    const tought = {
+      title: req.body.title,
+    }
+
+    try {
+      await Tought.update(tought, { where: {id: id} })
+
+      req.flash('message', 'Pensamento atualizado com sucesso!')
+  
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.log('Aconteceu um erro:' + error)
+    }
+
+  }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -8,6 +8,7 @@ const checkAuth = require('../helpers/auth').checkAuth
 router.get('/add', checkAuth, ToughtController.createTought)
 router.post('/add', checkAuth, ToughtController.createToughtSave)
 router.get('/edit/:id', checkAuth, ToughtController.updateTought)
+router.post('/edit', checkAuth, ToughtController.updateToughtSave)
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.post('/remove', checkAuth, ToughtController.removeTought)
 router.get('/', ToughtController.showToughts)

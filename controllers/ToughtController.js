6fe91a73b85b3a4205dbca48fea00cc0b58d4847
gabrielleeee <gabi.poletti2019@gmail.com>
2024-02-29 14:59:22 -0300

    feat: criação de funcionalidade de busca

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,16 +1,33 @@
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ToughtController {
   static async showToughts(req, res) {
 
+    let search = ''
+
+    if(req.query.search) {
+      search = req.query.search
+    }
+
     const toughtsData = await Tought.findAll({
       include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
     })
 
     const toughts = toughtsData.map((result) => result.get({ plain: true }))
 
-    res.render('toughts/home', { toughts })
+    let toughtsQty = toughts.length
+
+    if(toughtsQty === 0) {
+      toughtsQty = false
+    }
+
+    res.render('toughts/home', { toughts, search, toughtsQty })
   }
 
   static async dashboard (req, res) {
